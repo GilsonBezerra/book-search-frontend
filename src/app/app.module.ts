b976eda766a2e41cf176eb07bfc26645ab70cab0
Bezerra <809430@cognizant.com>
2019-09-17 10:53:27 -0300

    Front End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import {CalendarModule} from 'primeng/components/calendar/calendar';
+import {InputMaskModule} from 'primeng/inputmask';
+import {ButtonModule} from 'primeng/components/button/button';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BookSearchComponent } from './book-search/book-search.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BookSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    CalendarModule,
+    InputMaskModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
